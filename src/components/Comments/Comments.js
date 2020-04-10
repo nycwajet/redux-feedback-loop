0174df7c41beec45a2ca41cfa5011991fc6e4293
Henry Marin <henrymarin@rocketmail.com>
2020-04-10 16:39:59 -0500

    added Comments component skeleton

---
+++ src/components/Comments/Comments.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+
+class Comments extends Component{
+    render(){
+        return(
+            <section>
+                <h2>How Are You Feeling Today?</h2>
+                <p>In A Scale From 1 - 5</p>
+            </section>
+        )
+    }
+}
+
+export default connect() (Comments);
