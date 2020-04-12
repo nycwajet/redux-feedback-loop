a37eb34ccd3f8e5da1c1de98cfd112d28d1ac799
Henry Marin <henrymarin@rocketmail.com>
2020-04-11 19:20:42 -0500

    added handleSubmit and button to Feeling

--- src/components/Feeling/Feeling.js
+++ src/components/Feeling/Feeling.js
@@ -14,6 +14,17 @@ class Feeling extends Component{
         })
     }
 
+    handleSubmit=()=>{
+        console.log('In handleSubmit');
+        if (this.state.feeling !== ''){
+        const action= {type: 'ADD_FEELING', payload: this.state.feeling} ;
+        this.props.dispatch(action);
+        this.props.history.push('understanding');
+    } else {
+        alert('PLease Select an option');
+    }
+    }
+
 
     
     render(){
@@ -21,8 +32,10 @@ class Feeling extends Component{
             <section>
                 <h2>How Are You Feeling Today?</h2>
                 <p>In A Scale From 1 - 5</p>
+                <form onSubmit={this.handleSubmit}>
                 <input type="number" name="feeling" min="1" max="5" onChange={this.handleChange} />
                 <button type="submit">Next</button>
+                </form>
             </section>
         )
     }
