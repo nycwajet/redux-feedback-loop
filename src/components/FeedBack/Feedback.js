19c42d6472a6f757089c6f41cdce68a1e1d0bf53
Henry Marin <henrymarin@rocketmail.com>
2020-04-11 19:10:59 -0500

    added Feedback component

---
+++ src/components/FeedBack/Feedback.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {withRouter} from 'react-router-dom';
+
+class Feedback extends Component{
+    render(){
+        return(
+
+        )
+    }
+}
+
+export default connect()(withRouter(Feedback));
